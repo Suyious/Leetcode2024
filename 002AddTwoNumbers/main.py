744633d11fd246f019828d0fcc32ca6ed34eacfe
Suyash K. <suyu162001@gmail.com>
2024-01-25 19:38:27 +0530

    feat: implemented 002 in python3

--- 002AddTwoNumbers/main.py
+++ 002AddTwoNumbers/main.py
@@ -1,10 +1,64 @@
+from typing import Optional
+
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-def addTwoNumbers(l1, l2):
-    pass
+def printLinkedList(l: ListNode) -> None:
+    if(l != None):
+        print(l.val, end = " ")
+        printLinkedList(l.next)
+    else:
+        print()
+
+def listToLinkedList(list: ListNode) -> ListNode:
+    s = None
+    j = s
+    for l in list:
+        if(s == None):
+            s = ListNode(l)
+            j = s
+        else:
+            j.next = ListNode(l)
+            j = j.next
+    return s
+        
+
+def addTwoNumbers( l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    i = l1
+    j = l2
+    carry = 0
+    s = None
+    k = None
+
+    while i != None or j != None or carry != 0:
+        sum = carry
+
+        if i != None:
+            sum += i.val
+            i = i.next
+
+        if j != None:
+            sum += j.val
+            j = j.next
+
+        if s == None:
+            s = ListNode(sum % 10)
+            k = s
+        else:
+            k.next = ListNode(sum % 10)
+            k = k.next
+        
+        carry = sum // 10
+
+    return s
 
 if __name__ == '__main__':
-    print("Hello World\n")
+    l1 = listToLinkedList([1, 2, 3, 4])
+    l2 = listToLinkedList([2, 4, 6, 8])
+    s = addTwoNumbers(l1, l2)
+    printLinkedList(l1)
+    printLinkedList(l2)
+    printLinkedList(s)
