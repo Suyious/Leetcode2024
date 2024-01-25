35bf6058c5f15879349a337e2edc57a300d8b485
Suyash K. <suyu162001@gmail.com>
2024-01-25 14:34:26 +0530

    feat: Implemented 002 in C

--- 002AddTwoNumbers/main.c
+++ 002AddTwoNumbers/main.c
@@ -25,7 +25,37 @@ void deletel(struct ListNode* l) {
     }
 }
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *i = l1, *j = l2;
+    int carry = 0;
+    struct ListNode *s = NULL;
+    struct ListNode *k;
+
+    while(i != NULL || j != NULL || carry != 0) {
+        int sum = carry;
+
+        if(i != NULL) {
+            sum += i->val;
+            i = i->next;
+        }
+
+        if(j != NULL) {
+            sum += j->val;
+            j = j->next;
+        }
+
+        if(s == NULL) {
+            s = newl(sum % 10, NULL);
+            k = s;
+        } else {
+            k->next = newl(sum % 10, NULL);
+            k = k->next;
+        }
+        carry = sum / 10;
+    }
+
+    return s;
+};
 
 // One Dimentional Array to Singly linked list
 struct ListNode* artol(int a[], int size) {
@@ -54,9 +84,16 @@ void printl(struct ListNode* l) {
 
 int main() {
     int a[] = {1, 2, 3, 4, 5};
-    struct ListNode* l = artol(a, 5);
-    printl(l);
-    deletel(l);
+    int b[] = {2, 4, 6, 8, 9};
+    struct ListNode* l1 = artol(a, 5);
+    struct ListNode* l2 = artol(b, 5);
+    struct ListNode *sum = addTwoNumbers(l1, l2);
+    printl(l1);
+    printl(l2);
+    printl(sum);
+    deletel(l1);
+    deletel(l2);
+    deletel(sum);
 
     return 0;
 }
